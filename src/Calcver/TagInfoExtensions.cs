b9e9fada1ed0e763b8777a5cac09f3c043adce76
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2019-01-11 15:58:29 +0800

    wip

--- src/Calcver/TagInfoExtensions.cs
+++ src/Calcver/TagInfoExtensions.cs
@@ -16,6 +16,16 @@ namespace Calcver
         }
 
         public static IEnumerable<(TagInfo, SemanticVersion)> FilterVersionTags(this IEnumerable<TagInfo> tags)
-            => tags.Select(t => (t, t.GetVersion())).Where(t => t.Item2 != null);
+            =>  tags.Select(t => (t, t.GetVersion())).Where(t => t.Item2 != null);
+
+        public static string GetScores(IEnumerable<int> a, IEnumerable<int> b) {
+            var (scoreA,scoreB) = a.Zip(b, (cardA, cardB) => (cardA, cardB))
+                .SkipWhile((s, i) => i % 2 == 0)
+                .Aggregate((0, 0), (scores, i) => (i.cardA - i.cardB, i.cardB - i.cardA));
+
+            if (scoreA == scoreB)
+                return "Tie";
+            return scoreA > scoreB ? "A" : "B";
+        }
     }
 }
