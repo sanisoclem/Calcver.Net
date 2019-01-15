18fcea04f11bd714c0d79806b496d84550e08fef
Jerahmeel Cosinas <me@jerahmeelcosinas.net>
2019-01-16 01:38:36 +0800

    test: test scenarios for version calculation

--- tests/Calcver.Tests/Helpers/RepositoryTestHelpers.cs
+++ tests/Calcver.Tests/Helpers/RepositoryTestHelpers.cs
@@ -2,9 +2,12 @@
 using NSubstitute;
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Calcver.Tests.Helpers {
     public static class RepositoryTestHelpers {
+        static readonly Regex historyRegex = new Regex(@"(?'sha'\w+) \((?'tag'v?[0-9\.]+)?\) (?'msg'.*)");
+
         public static string CreateMockCommits(this IRepository repo, string lastTag, int numCommits = 0, string commitMessage = null)
         {
             var f = new Fixture();
@@ -25,6 +28,46 @@ namespace Calcver.Tests.Helpers {
             repo.GetCommits(lastTag).Returns(commits);
 
             return commits.LastOrDefault()?.Id ?? tagCommitId;
+        }
+
+        public static string CreateHistory(this IRepository repo, string historyDescription)
+        {
+            // -- parse the history data
+            var hist = historyDescription.Split("\n\r".ToCharArray(), StringSplitOptions.RemoveEmptyEntries) // split by lines
+                .Select(line => historyRegex.Match(line.Trim()))
+                .Select((r,i) => new {
+                    Index = i,
+                    Sha = r.Groups["sha"].Value,
+                    Tag = r.Groups["tag"].Value,
+                    Msg = r.Groups["msg"].Value
+                }).Reverse().ToList();
+
+            // -- compute commits to return
+            repo.GetCommits(Arg.Any<string>(), Arg.Any<string>()).Returns(c => {
+                var qry = hist.AsEnumerable();
+                if (c[0] is string since) {
+                    var commit = qry.Single(a => a.Tag == since);
+                    qry = qry.SkipWhile(a => a.Index >= commit.Index);
+                }
+                if (c[1] is string until) {
+                    var commit = qry.Single(a => a.Tag == until);
+                    qry = qry.Where(a => a.Index >= commit.Index);
+                }
+                return qry.Select(a => new CommitInfo {
+                    Id = a.Sha,
+                    Message = a.Msg.Replace(@"\n", "\n")
+                });
+            });
+
+            repo.GetTags().Returns(c => hist.Where(a => !string.IsNullOrEmpty(a.Tag)).Select(a => new TagInfo {
+                Commit = new CommitInfo {
+                    Id = a.Sha,
+                    Message = a.Msg
+                },
+                Name = a.Tag
+            }));
+
+            return hist.Last().Sha;
         }
     }
 }
